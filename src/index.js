abb5de05df858c7692a302fab8aaec250a60e161
SHAILENDRA SHARMA <shailendra.sharma@gmail.com>
2016-03-22 01:17:21 +0530

    1) Move files out of api folder. 2) Export all from index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.exports.RedisClient = require('./RedisClient');
+module.exports.Request = require('./Request');
+module.exports.ExtensibleError = require('./ExtensibleError');
+module.exports.ValidationError = require('./ValidationError');
+module.exports.InternalServiceError = require('./InternalServiceError');
