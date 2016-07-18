64a8a5107287868d90f41e3690f400fd7151eae4
SHAILENDRA SHARMA <shailendra.sharma@gmail.com>
2016-07-18 20:56:15 +0530

    Move handleResponse and handleResponseArray methods to node commons...

--- src/Request.js
+++ src/Request.js
@@ -3,8 +3,25 @@ import Http from 'http';
 import Https from 'https';
 import Promise from 'bluebird';
 import Request from 'request';
+import InternalServiceError from './InternalServiceError';
+import Chalk from 'chalk';
 
-export default (config) => {
+const SUCCESS_STATUS = 'SUCCESS';
+const FAIL_STATUS = 'FAIL';
+
+const TRACE_LOG_LEVEL = 'trace';
+const DEBUG_LOG_LEVEL = 'debug';
+const INFO_LOG_LEVEL = 'info';
+
+const LogLevels = {
+    [TRACE_LOG_LEVEL]: 0,
+    [DEBUG_LOG_LEVEL]: 1,
+    [INFO_LOG_LEVEL]: 2
+};
+
+const HEAD_HTTP_METHOD = 'HEAD';
+
+export function builder(config) {
     let Agent = null;
     if (config.https) {
         Agent = Https.Agent;
@@ -22,4 +39,65 @@ export default (config) => {
     const params = _.defaultsDeep(config, {json: true, gzip: true, agent: keepAliveAgent, time: config.logLevel === 'trace'});
 
     return Promise.promisify(Request.defaults(params));
-};
+}
+
+export function handleResponse(response, okStatusCodes, operation, logLevel) {
+    if (!response) {
+        return Promise.reject('ERROR: No Response');
+    }
+
+    if (_.isArray(response)) {
+        response = response[0];
+    }
+
+    if (_.get(LogLevels, logLevel) <= _.get(LogLevels, DEBUG_LOG_LEVEL) || (response.statusCode >= 400 && !_.get(okStatusCodes, response.statusCode) && response.request.method !== HEAD_HTTP_METHOD)) {
+        console.log();
+        console.log(Chalk.blue('------------------------------------------------------'));
+        console.log(Chalk.blue.bold(`${response.request.method} ${response.request.href}`));
+
+        const format = response.statusCode < 400 ? Chalk.green : Chalk.red;
+
+        console.log(format(`Status: ${response.statusCode}, Elapsed Time: ${response.elapsedTime}`));
+
+        if (response.request.method !== HEAD_HTTP_METHOD) {
+            console.log(format(JSON.stringify(response.body, null, 2)));
+        }
+
+        console.log(Chalk.blue('------------------------------------------------------'));
+        console.log();
+    }
+
+    if (response.statusCode < 400 || _.get(okStatusCodes, response._statusCode)) {
+        return _.extend({
+            _statusCode: response.statusCode,
+            _status: response.statusCode < 400 ? SUCCESS_STATUS : FAIL_STATUS,
+            _elapsedTime: response.elapsedTime,
+            _operation: operation
+        }, response.body);
+    }
+
+    throw new InternalServiceError('Internal Service Error', {
+        _statusCode: response.statusCode, details: response.body && response.body.error || response.body
+    });
+}
+
+export function handleResponseArray(responses, okStatusCodes, operation, logLevel = INFO_LOG_LEVEL) {
+    return Promise
+      .all(_.map(responses, response => {
+          let promise = null;
+          try {
+              promise = Promise.resolve(handleResponse(response, okStatusCodes, operation, logLevel));
+          } catch (error) {
+              promise = Promise.reject(error);
+          }
+
+          return promise.reflect();
+      }))
+      .map(inspection => {
+          if (inspection.isFulfilled()) {
+              return inspection.value();
+          }
+
+          return inspection.reason();
+      });
+}
